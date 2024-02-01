3c01e216a95e5b09d969b399064d3dcb1116e4e9
Prachanda Rana <62231260+prachanda482244@users.noreply.github.com>
2024-02-01 21:58:52 +0545

    create service page for portfolio

--- PORTFOLIO-WEB/src/App.jsx
+++ PORTFOLIO-WEB/src/App.jsx
@@ -1,6 +1,7 @@
 import Navbar from "./Components/Navbar";
 import Parallax from "./Components/Parallax";
 import Hero from "./Pages/Hero";
+import Services from "./Pages/Services";
 import planets from './assets/planets.png'
 import sun from './assets/sun.png'
 
@@ -12,7 +13,7 @@ const App = () => {
       <Hero />
     </section>
     <section id="services"><Parallax heading="What I do" gradient="#0c0c1d" bg={planets} /></section>
-    <section>Services</section>
+    <section><Services /></section>
     <section id="services"><Parallax heading="What I did" gradient="#505064" bg={sun} /></section>
     <section id="about">About</section>
     <section id="skills">skills</section>
