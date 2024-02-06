3a479d017fdb368ba51a276fb3e583f030ecb48c
Prachanda Rana <62231260+prachanda482244@users.noreply.github.com>
2024-02-06 19:25:42 +0545

    complete the transition

--- PORTFOLIO-WEB/src/Components/SingleItem.jsx
+++ PORTFOLIO-WEB/src/Components/SingleItem.jsx
@@ -1,21 +1,22 @@
 import { FaGithub } from "react-icons/fa";
 import { CiGlobe } from "react-icons/ci";
-import { useScroll, motion, useTransform } from "framer-motion"
+import { useScroll, motion, useTransform, transform } from "framer-motion"
 import { useRef } from "react"
 import { Link } from "react-router-dom"
 const SingleItem = ({ projectItems }) => {
     const ref = useRef()
-    const { scrollYProgress } = useScroll({ ref: ref })
+    const { scrollYProgress } = useScroll({ target: ref })
+
     const y = useTransform(scrollYProgress, [0, 1], [-300, 300])
 
     return (
-        <section ref={ref}>
-            <div className="container flex items-center h-full w-full justify-center">
+        <section >
+            <div className="overflow-hidden flex items-center h-full w-full justify-center">
                 <div className="max-w-6xl h-full m-auto items-center justify-center flex gap-10">
-                    <div className="flex-1 h-1/2">
-                        <img src={projectItems.img} className="h-full w-full object-cover" alt="Project Image" />
+                    <div ref={ref} className="flex-1 h-1/2 hover:rounded-lg overflow-hidden ">
+                        <img src={projectItems.img} className="h-full transition-all delay-75 end-auto  hover:scale-110 w-full object-cover" alt="Project Image" />
                     </div>
-                    <motion.div className="textContainer flex flex-col gap-6 flex-1">
+                    <motion.div style={{ y }} className="textContainer flex flex-col gap-6 flex-1">
                         <h1 style={{ textShadow: "4px 5px 5px blue" }} className="text-4xl font-bold text-white " >{projectItems.title}</h1>
                         <p className="text-gray-500 text-lg">{projectItems.description}</p>
                         <div className="flex  items-center gap-4">
