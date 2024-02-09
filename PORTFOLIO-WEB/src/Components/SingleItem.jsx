d00f05b8148ed90d3a0e350a47f90ec1b35ebcc1
Prachanda Rana <62231260+prachanda482244@users.noreply.github.com>
2024-02-09 15:10:42 +0545

    complete project section

--- PORTFOLIO-WEB/src/Components/SingleItem.jsx
+++ PORTFOLIO-WEB/src/Components/SingleItem.jsx
@@ -6,31 +6,28 @@ import { Link } from "react-router-dom"
 const SingleItem = ({ projectItems }) => {
     const ref = useRef()
     const { scrollYProgress } = useScroll({ target: ref })
+    const x = useTransform(scrollYProgress, [0, 1], [-300, 300])
 
-    const y = useTransform(scrollYProgress, [0, 1], [-300, 300])
-
-    const opacity = useTransform(scrollYProgress, [.5, 0], [1, 0]);
 
     return (
-        <section className="h-auto md:h-[100vh]" >
-            <div className="overflow-hidden mt-40 flex items-center h-full w-full justify-center">
-                <div className="max-w-6xl h-full  mt-28  md:mt-0 md:m-auto items-center justify-center flex-col md:flex-row flex gap-10">
+        <section className="h-auto md:h-[100vh]  " >
+            <div className="overflow-hidden  h-full w-full">
+                <div className="max-w-6xl h-full p-6 md:p-10 m-auto items-center md:items-center justify-center flex-col md:flex-row flex md:gap-10">
                     <motion.div
-                        style={{ opacity }}
-                        ref={ref} className="flex-1 h-1/2 hover:rounded-lg md:overflow-hidden ">
-                        <img src={projectItems.img} className="h-full transition-all delay-75 end-auto  hover:scale-110 w-full object-cover" alt="Project Image" />
+                        ref={ref} className="flex-1 w-full max-h-80 md:h-1/2 hover:rounded-lg overflow-hidden ">
+                        <img src={projectItems.img} className="h-full  transition-all delay-75 end-auto  hover:scale-110 w-full object-contain md:object-cover" alt="Project Image" />
                     </motion.div>
                     <motion.div
-                        style={{ y }}
-                        className="textContainer flex flex-col gap-6 flex-1">
+                        style={{ x }}
+                        className="textContainer max-sm:!transform-none items-center md:items-start flex flex-col gap-4 md:gap-6 flex-1">
                         <h1 style={{ textShadow: "4px 5px 5px blue" }} className="text-2xl md:text-4xl font-bold text-white " >{projectItems.title}</h1>
                         <p className="text-gray-500 text-xs md:text-lg">{projectItems.description}</p>
                         <div className="flex  items-center gap-4">
-                            <Link to={projectItems.github} target="_blank" className="flex items-center gap-2 bg-orange-600 hover:bg-transparent border border-orange-600 hover:border-white hover:text-blue-300 transition-all delay-75 ease-linear py-2 px-6 rounded-lg  font-semibold">
+                            <Link to={projectItems.github} target="_blank" className="flex items-center gap-2 bg-orange-600 hover:bg-transparent border border-orange-600 hover:border-white hover:text-blue-300 transition-all delay-75 ease-linear py-1 md:py-2 px-6 rounded-lg  font-semibold">
                                 Github
                                 <FaGithub className="h-5 w-5" />
                             </Link>
-                            <Link to={projectItems.live} target="_blank" className="flex items-center gap-2 border-white border py-2 px-6 rounded-lg hover:bg-blue-600 hover:border-blue-600  transition-all delay-75 ease-linear font-semibold">Live
+                            <Link to={projectItems.live} target="_blank" className="flex items-center gap-2 border-white border  py-1 md:py-2 px-6 rounded-lg hover:bg-blue-600 hover:border-blue-600  transition-all delay-75 ease-linear font-semibold">Live
                                 <CiGlobe className="h-5 w-5" />
                             </Link>
                         </div>
--- PORTFOLIO-WEB/src/Pages/Projects.jsx
+++ PORTFOLIO-WEB/src/Pages/Projects.jsx
@@ -15,9 +15,9 @@ const Projects = () => {
 
     return (
         <div className="portfolio relative " id="projects" ref={ref}>
-            <div className="progress sticky top-0 left-0 p-10 text-center text-orange-500 text-3xl">
-                <h1>Featured Works</h1>
-                <motion.div style={{ scaleX }} className="progress-bar  bg-white h-2 rounded-lg"></motion.div>
+            <div className="progress sticky top-0 left-0 z-10  md:pt-10 text-center text-2xl pt-[calc(100vh-60px)]   md:text-4xl text-white font-semibold md:text-orange-500 ">
+                <h1 className=" mb-2">Featured Works</h1>
+                <motion.div style={{ scaleX }} className="progress-bar  bg-white h-1 md:h-2 rounded-lg"></motion.div>
             </div>
 
             {
