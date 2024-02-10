2e57e2c8c75ca4e1ea056e3c418b20f5c3b9fe50
Prachanda Rana <62231260+prachanda482244@users.noreply.github.com>
2024-02-10 18:15:45 +0545

    update box shadow

--- PORTFOLIO-WEB/src/Components/TagComponent.jsx
+++ PORTFOLIO-WEB/src/Components/TagComponent.jsx
@@ -1,8 +1,8 @@
 
 const TagComponent = ({ title }) => {
     return (
-        <div style={{ boxShadow: "inset 2px 2px 10px black " }} className='sticky bg-slate-300 text-slate-800 z-10 top-0 left-0'>
-            <h2 style={{ textShadow: '2px 2px 10px black' }} className="text-center text-4xl md:text-5xl uppercase font-semibold p-2">&lt;{title} /&gt;</h2>
+        <div style={{ boxShadow: "inset 2px 2px 400px white " }} className='sticky bg-slate-300 text-slate-800 z-10 top-0 left-0'>
+            <h2 style={{ textShadow: '1px 2px 2px black' }} className="text-center text-4xl md:text-5xl uppercase font-semibold p-2">&lt;{title} /&gt;</h2>
         </div>
     )
 }
